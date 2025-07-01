753fa2b039acefc25d0f088e9b280f488ca3d55d
fahrulrozirgb <ac9613522@gmail.com>
2025-07-01 18:30:57 +0700

    Fix: base path untuk Vercel build

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,8 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
-const isProd = process.env.NODE_ENV === "production";
-
 export default defineConfig({
-  base: isProd ? "/Portofolio/" : "/", // ✅ hanya pakai base path saat build
+  base: "./", // ✅ fix untuk jalur Vercel
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
